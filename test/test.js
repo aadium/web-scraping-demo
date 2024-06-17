0ce4787a96f185564a93a476bebc3bc060f42276
Aadi Umrani <aadi.p.um@gmail.com>
2024-06-17 08:47:06 -0400

    chore: Remove test files

--- test/test.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const { createClient } = require('@supabase/supabase-js');
-
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY)
-
-supabase.functions.invoke('scraper', { body: { id: '93f272d3-b2db-42df-b89d-d45843f28ca7' }})
-    .then(({ data, error }) => {
-        console.log(data)
-    });
