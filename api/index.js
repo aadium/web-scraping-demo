a01aad141528f321f863fe1d02590aeee0c21363
Aadi Umrani <aadi.p.um@gmail.com>
2024-06-24 07:19:06 -0400

    chore: Update CORS configuration for API

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://scrapify-web.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://scrapify-web.vercel.app'],
 }));
 
 app.use('/auth', authRoutes);
