5d3d88c28623e92c0705c6c8ec6ce0c94aef8111
Aadi Umrani <aadi.p.um@gmail.com>
2024-06-23 21:03:08 -0400

    chore: Add root route to API

--- api/index.js
+++ api/index.js
@@ -12,5 +12,9 @@ app.use(cors());
 app.use('/auth', authRoutes);
 app.use('/scraper', scraperRoutes);
 
+app.get('/', (req, res) => {
+    res.json({ message: 'Web Scraper Demo API' });
+});
+
 const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`Server running on port ${port}`));
